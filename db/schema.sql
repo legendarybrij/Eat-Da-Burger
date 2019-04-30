dea3451030bddc8f2227e04d4d8c80c24be09f59
Brijesh Patel <Brij@Brijesh-Patels-MacBook-Pro.local>
2019-04-29 21:18:30 -0400

    JAW update

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,31 @@
--- ### Schema
+CREATE DATABASE IF NOT EXISTS burgers_db;
+USE burgers_db;
 
-CREATE DATABASE cat_db;
-USE cat_db;
+-- If the table already exists, remove it before trying to create the table again
+DROP TABLE IF EXISTS burgers;
 
-CREATE TABLE cats
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+-- Create the burgers table
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    sleepy BOOL DEFAULT false,
+    PRIMARY KEY (id)
 );
 
-USE cat_db;
-select * from cat_db;
+USE burgers_db;
+CREATE TABLE todos(
+    id INT AUTO_INCREMENT NOT NULL,
+    description VARCHAR(255),
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
+);
+
+USE burgers_db;
+
+
+INSERT INTO burgers (burger_name,sleepy) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, sleepy) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, sleepy) VALUES ('Pizza Burger', FALSE);
+
+USE burgers_db;
+select * from burgers;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,9 @@
-USE cat_db;
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+USE burgers_db;
+
+
+INSERT INTO burgers (burger_name,sleepy) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, sleepy) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, sleepy) VALUES ('Pizza Burger', FALSE);
+
+USE burgers_db;
+select * from burgers;
